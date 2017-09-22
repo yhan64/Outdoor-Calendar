488ee106f1f6a7e856260c6d9d66aa22ebc47999
yhan <a0152497@gmail.com>
2017-09-21 21:06:56 -0700

    move in all work from oc3

---
+++ oc/js/actions/appActions.js
@@ -0,0 +1,43 @@
+import moment from 'moment'
+import * as ActionTypes from '../constants/ActionTypes';
+
+export function showEventDetailsScreen() {
+  return {
+    type: ActionTypes.SHOW_EVENT_DETAILS_MODAL
+  }
+}
+
+export function closeEventDetailsScreen() {
+  return {
+    type: ActionTypes.CLOSE_EVENT_DETAILS_MODAL
+  }
+}
+
+export function getEventsOnDays(events) {
+  return (dispatch, getState) => {
+    const currEvents = events || getState().api.events
+    let eventsOnDays = {}
+    currEvents.forEach((event) => {
+      const startDate = moment(event.start_date)
+      const endDate = moment(event.end_date)
+      for(let currDate = startDate; currDate.diff(endDate) <= 0; currDate.add(1, 'days')){
+        const formattedCurrDate = currDate.format('YYYY-MM-DD')
+        if(!eventsOnDays[formattedCurrDate]) {
+          eventsOnDays[formattedCurrDate] = []
+        }
+        eventsOnDays[formattedCurrDate].push({
+          title: event.title || event.description,
+          description: event.description,
+        })
+      }
+    });
+    dispatch(getEventsOnDaysAction(eventsOnDays))
+  }
+}
+
+function getEventsOnDaysAction(eventsOnDays) {
+  return {
+    type: ActionTypes.EVENTS_ON_DAYS_CONVERTED,
+    eventsOnDays
+  }
+}
---
+++ oc/js/screens/EventDetailsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+
+function EventDetailsScreen() {
+
+}
+
+export default EventDetailsScreen
