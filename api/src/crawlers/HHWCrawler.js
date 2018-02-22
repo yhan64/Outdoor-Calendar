2390841bb792126e4bfaa7257715f0002a08bec2
yhan <a0152497@gmail.com>
2018-02-22 11:51:21 -0800

    add dev env while build

--- api/src/crawlers/HHWCrawler.js
+++ api/src/crawlers/HHWCrawler.js
@@ -3,6 +3,5 @@ const cheerioAdv = require('cheerio-advanced-selectors')
 const cheerio = cheerioAdv.wrap(require('cheerio'))
 
 export default class HHWCrawler {
-  startUrl = 'http://www.honghuworld.com/activity'
 
 }
